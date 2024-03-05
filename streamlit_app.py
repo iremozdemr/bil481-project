5688830cec14950c937ed2dcce050aaab7ad12ee
İrem Özdemir <irem.ozdemir@etu.edu.tr>
2024-03-05 17:47:34 +0300

    trying streamlit

---
+++ streamlit_app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import base64
+
+st.write("hello world")
+st.title("our bil481 project")
+
+#with open('style.css') as f:
+#    css = f.read()
+
+#st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+
+#video_file = open('background.mp4', 'rb')
+#video_bytes = video_file.read()
+#st.video(video_bytes)
