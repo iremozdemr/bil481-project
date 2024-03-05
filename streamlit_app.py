7fbb2bc9dcb547a77d7d188fbe3b8835c738ea74
İrem Özdemir <irem.ozdemir@etu.edu.tr>
2024-03-05 19:25:07 +0300

    added map

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,61 @@
 import streamlit as st
-import base64
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import requests
 
-st.write("hello world")
-st.title("our bil481 project")
+st.title(":blue[**our bil481 project**]")
+
+option = st.selectbox('please choose the country for flight information:',('america', 'turkey', 'france'))
+
+st.write('you selected:', option)
+
+arr = np.random.normal(1, 1, size=100)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
+
+st.pyplot(fig)
+
+# API endpoint URL
+url = "https://api.adsb.lol/v2/ladd"
+
+# GET isteği gönderme
+response = requests.get(url)
+
+# DataFrame'i oluşturmak için boş bir liste oluşturalım
+aircraft_list = []
+
+# Yanıtı kontrol etme
+if response.status_code == 200:
+    # Yanıtı JSON formatına dönüştürme
+    data = response.json()
+
+    # Uçaklar listesini alma
+    aircrafts = data.get("ac", [])
+
+    # Her bir uçak için bilgileri liste içine ekleme
+    for aircraft in aircrafts:
+        lat = aircraft.get("lat", 0)
+        lon = aircraft.get("lon", 0)
+        
+        # Her bir uçağın bilgilerini bir sözlükte toplama
+        aircraft_info = {
+            "lat": lat,
+            "lon": lon
+        }
+        
+        # Her bir uçağın bilgilerini liste içine ekleme
+        aircraft_list.append(aircraft_info)
+
+    # Liste ile DataFrame oluşturma
+    df = pd.DataFrame(aircraft_list)
+
+    st.map(df)
+    # DataFrame'i yazdırma
+    print(df)
+else:
+    print("API'ye erişimde bir hata oluştu. Hata kodu:", response.status_code)
+
 
 #with open('style.css') as f:
 #    css = f.read()
