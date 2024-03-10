338fc650bdcb5dec77a0622fa1e9e2d0ec5f9837
eyllcintiriz <127094591+eyllcintiriz@users.noreply.github.com>
2024-03-10 12:15:57 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,89 +5,100 @@ import pandas as pd
 import requests
 import time
 
-st.title(":blue[**our bil481 project**]")
+from about_us_content import about_us_content
+from about_us_content import about_our_project
 
-option = st.selectbox('please choose the country for flight information:',('america', 'turkey', 'france'))
+st.sidebar.title("Navigation")
+page = st.sidebar.selectbox("Select Page", ("About us","About our project", "Main page"))
 
-st.write('you selected:', option)
+about_us_content(page)
+about_our_project(page)
 
-# API endpoint URL
-url = "https://api.adsb.lol/v2/ladd"
+if page == "Main page":
 
-response = requests.get(url)
-
-# Yanıtı kontrol etme
-if response.status_code == 200:
-    # Yanıtı JSON formatına dönüştürme
-    data = response.json()
-
-    # Uçaklar listesini alma
-    aircrafts = data.get("ac", [])
-
-    # Uçakların enlem ve boylam bilgilerini saklayacak boş listeler oluşturalım
-    latitudes = []
-    longitudes = []
-
-    # Her bir uçak için enlem ve boylam bilgilerini alıp listelere ekleyelim
-    for aircraft in aircrafts:
-        lat = aircraft.get("lat", 0)
-        lon = aircraft.get("lon", 0)
-        latitudes.append(lat)
-        longitudes.append(lon)
-
-    # Scatter plot oluşturma
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ax.scatter(longitudes, latitudes, marker='o', color='blue')
-    ax.set_xlabel('Boylam')
-    ax.set_ylabel('Enlem')
-    ax.set_title('Uçakların Konumu')
-    ax.grid(True)
+    st.title(":blue[**our bil481 project**]")
     
-    # Matplotlib figürünü Streamlit'te gösterme
-    st.pyplot(fig)
-else:
-    st.error("API'ye erişimde bir hata oluştu. Hata kodu:" + str(response.status_code))
-
-@st.cache_data(ttl=10)  # 10 saniye cache süresi
-def get_aircraft_data():
-    # GET isteği gönderme
+    option = st.selectbox('please choose the country for flight information:',('america', 'turkey', 'france'))
+    
+    st.write('you selected:', option)
+    
+    # API endpoint URL
+    url = "https://api.adsb.lol/v2/ladd"
+    
     response = requests.get(url)
-
-    aircraft_list = []
-
+    
     # Yanıtı kontrol etme
     if response.status_code == 200:
         # Yanıtı JSON formatına dönüştürme
         data = response.json()
-
+    
         # Uçaklar listesini alma
         aircrafts = data.get("ac", [])
-
-        # Her bir uçak için bilgileri liste içine ekleme
+    
+        # Uçakların enlem ve boylam bilgilerini saklayacak boş listeler oluşturalım
+        latitudes = []
+        longitudes = []
+    
+        # Her bir uçak için enlem ve boylam bilgilerini alıp listelere ekleyelim
         for aircraft in aircrafts:
             lat = aircraft.get("lat", 0)
             lon = aircraft.get("lon", 0)
-            
-            # Her bir uçağın bilgilerini bir sözlükte toplama
-            aircraft_info = {
-                "lat": lat,
-                "lon": lon
-            }
-            
-            # Her bir uçağın bilgilerini liste içine ekleme
-            aircraft_list.append(aircraft_info)
-
-    return aircraft_list
-
-# İlk haritayı oluşturmak için boş bir container oluşturalım
-map_container = st.empty()
-
-while True:
-    aircraft_list = get_aircraft_data()
-    df = pd.DataFrame(aircraft_list)
+            latitudes.append(lat)
+            longitudes.append(lon)
     
-    # Önceki haritayı temizleyip yeni haritayı gösterme
-    with map_container:
-        st.map(df)
+        # Scatter plot oluşturma
+        fig, ax = plt.subplots(figsize=(10, 6))
+        ax.scatter(longitudes, latitudes, marker='o', color='blue')
+        ax.set_xlabel('Boylam')
+        ax.set_ylabel('Enlem')
+        ax.set_title('Uçakların Konumu')
+        ax.grid(True)
+        
+        # Matplotlib figürünü Streamlit'te gösterme
+        st.pyplot(fig)
+    else:
+        st.error("API'ye erişimde bir hata oluştu. Hata kodu:" + str(response.status_code))
     
-    time.sleep(10)  # 10 saniye bekleme süresi
+    @st.cache_data(ttl=10)  # 10 saniye cache süresi
+    def get_aircraft_data():
+        # GET isteği gönderme
+        response = requests.get(url)
+    
+        aircraft_list = []
+    
+        # Yanıtı kontrol etme
+        if response.status_code == 200:
+            # Yanıtı JSON formatına dönüştürme
+            data = response.json()
+    
+            # Uçaklar listesini alma
+            aircrafts = data.get("ac", [])
+    
+            # Her bir uçak için bilgileri liste içine ekleme
+            for aircraft in aircrafts:
+                lat = aircraft.get("lat", 0)
+                lon = aircraft.get("lon", 0)
+                
+                # Her bir uçağın bilgilerini bir sözlükte toplama
+                aircraft_info = {
+                    "lat": lat,
+                    "lon": lon
+                }
+                
+                # Her bir uçağın bilgilerini liste içine ekleme
+                aircraft_list.append(aircraft_info)
+    
+        return aircraft_list
+    
+    # İlk haritayı oluşturmak için boş bir container oluşturalım
+    map_container = st.empty()
+    
+    while True:
+        aircraft_list = get_aircraft_data()
+        df = pd.DataFrame(aircraft_list)
+        
+        # Önceki haritayı temizleyip yeni haritayı gösterme
+        with map_container:
+            st.map(df)
+        
+        time.sleep(10)  # 10 saniye bekleme süresi
